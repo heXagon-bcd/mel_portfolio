0c3ad76ce83ce4023147accff5788c7c898524be
Shawn He <shawnhe1991@gmail.com>
2024-12-25 17:42:41 -0500

    complete static header portion

--- src/slices/HomeHeader/index.js
+++ src/slices/HomeHeader/index.js
@@ -7,9 +7,11 @@
 import Image from "next/image";
 
 const HomeHeader = ({ slice }) => {
-  console.log(slice.primary.bgimage[0].bgimage.url);
+  console.log("hello", slice.primary.bgimage[0].bgimage2.url);
+
   return (
     <section
+      className="w-screen"
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
@@ -17,27 +19,88 @@ const HomeHeader = ({ slice }) => {
         {slice.primary.header}
       </div>
       <div
-        className="min-h-screen bg-center absolute"
+        className="min-h-screen bg-center relative z-0"
         style={{
           backgroundImage: `url(${slice.primary.bgimage[0].bgimage.url})`,
           backgroundSize: "cover",
         }}
       >
-        <div className="relative">
+        <div className="">
           {slice.primary.images.map((item, index) => {
-            return (
-              <div key={index} className="">
-                <Image
-                  index={index}
-                  src={item.image.url}
-                  alt="header iamge"
-                  width={item.image.dimensions.width}
-                  height={item.image.dimensions.height}
-                />
-              </div>
-            );
+            if (index === 0) {
+              return (
+                <div key={index} className="absolute top-20 left-28 z-10">
+                  <Image
+                    index={index}
+                    src={item.image.url}
+                    alt="header iamge"
+                    width={item.image.dimensions.width}
+                    height={item.image.dimensions.height}
+                  />
+                </div>
+              );
+            }
+            if (index === 1) {
+              return (
+                <div key={index} className="absolute top-5 right-80">
+                  <Image
+                    index={index}
+                    src={item.image.url}
+                    alt="header iamge"
+                    width={item.image.dimensions.width}
+                    height={item.image.dimensions.height}
+                  />
+                </div>
+              );
+            }
+            if (index === 2) {
+              return (
+                <div key={index} className="absolute right-10 top-36 z-20">
+                  <Image
+                    index={index}
+                    src={item.image.url}
+                    alt="header iamge"
+                    width={item.image.dimensions.width}
+                    height={item.image.dimensions.height}
+                  />
+                </div>
+              );
+            }
+            if (index === 3) {
+              return (
+                <div key={index} className="absolute right-52 top-72 ">
+                  <Image
+                    index={index}
+                    src={item.image.url}
+                    alt="header iamge"
+                    width={item.image.dimensions.width}
+                    height={item.image.dimensions.height}
+                  />
+                </div>
+              );
+            }
+            if (index === 4) {
+              return (
+                <div key={index} className="absolute left-10 bottom-80 ">
+                  <Image
+                    index={index}
+                    src={item.image.url}
+                    alt="header iamge"
+                    width={item.image.dimensions.width}
+                    height={item.image.dimensions.height}
+                  />
+                </div>
+              );
+            }
           })}
         </div>
+        <div
+          className="h-20 bg-center bottom-0  z-0"
+          style={{
+            backgroundImage: `url(${slice.primary.bgimage[0].bgimage2.url})`,
+            backgroundSize: "cover",
+          }}
+        />
       </div>
     </section>
   );
